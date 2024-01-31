6e2a5d7561fc215784fa7ab646af8ec20f3fc65b
Nicolai Haug <nicolaihaug@yahoo.com>
2024-01-31 01:53:25 +0100

    Test for checking correct installation of env

---
+++ tests/test_imports.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pytest  # noqa
+
+
+def test_import_numpy():
+    import numpy
+
+
+def test_import_matplotlib():
+    import matplotlib
+
+
+def test_import_scipy():
+    import scipy
+
+
+def test_import_pandas():
+    import pandas
+
+
+def test_import_mpi4py():
+    import mpi4py
+
+
+def test_import_pymc():
+    import pymc
+
+
+def test_import_seaborn():
+    import seaborn
+
+
+def test_import_arviz():
+    import arviz
+
+
+def test_import_rich():
+    import rich
+
+
+def test_import_sklearn():
+    import sklearn
+
+
+def test_import_skimage():
+    import skimage
+
+
+def test_import_jax():
+    import jax
+
+
+def test_import_jaxtyping():
+    import jaxtyping
+
+
+def test_import_chex():
+    import chex
+
+
+def test_import_flax():
+    import flax
+
+
+def test_import_equinox():
+    import equinox
+
+
+def test_import_jaxopt():
+    import jaxopt
+
+
+def test_import_optax():
+    import optax
+
+
+def test_import_distrax():
+    import distrax
+
+
+def test_import_blackjax():
+    import blackjax
+
+
+def test_import_numpyro():
+    import numpyro
+
+
+def test_import_clu():
+    import clu
+
+
+def test_import_torch():
+    import torch
+
+
+def test_import_torchvision():
+    import torchvision
+
+
+def test_import_torchaudio():
+    import torchaudio
+
+
+def test_import_pyro():
+    import pyro
+
+
+def test_import_tensorflow():
+    import tensorflow
+
+
+def test_import_tensorflow_datasets():
+    import tensorflow_datasets
+
+
+def test_import_einops():
+    import einops
+
+
+def test_import_h5py():
+    import h5py
+
+
+def test_import_tqdm():
+    import tqdm
