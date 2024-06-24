c9b57ec972cfbfa3779c7ae5a0efae58e261bd3d
Nicolai Haug <nicolaihaug@yahoo.com>
2024-06-24 02:30:24 +0200

    Remove tight_layout

--- src/neurovae/utils.py
+++ src/neurovae/utils.py
@@ -40,7 +40,7 @@ def plot_digits(imgs, labels=None, dim=None, axes=None, figsize=(5, 5), cmap="gr
         gridspec_kw = {"hspace": 0.05, "wspace": 0.05}
         if labels is not None:
             gridspec_kw["hspace"] = 0.3
-        fig, axes = plt.subplots(dim, dim, figsize=figsize, tight_layout=True, gridspec_kw=gridspec_kw)
+        fig, axes = plt.subplots(dim, dim, figsize=figsize, gridspec_kw=gridspec_kw)
 
     for n in range(dim**2):
         img = imgs[n]
